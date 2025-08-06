7aea22e74bc10d695bed60e29f67bbb987aab9f8
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-07 04:57:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ const winPatterns = [
 boxes.forEach((box) => {
     box.addEventListener("click", ()=>{
         console.log("box was clicked")
-        box.innerText = "X"
+        box.innerText = "X";
     });
 });
