161e030d475878ffa3377862f8050d302a7358e1
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-07 05:18:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const checkWinner = () => {
 
         if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
             if(pos1Val == pos2Val && pos2Val == pos3Val){
-                console.log("Winner")
+                console.log("Winner");
             }
         }
     }
