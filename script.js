6778b5dd7df16ab00eb4e2d29b755d8b54d8bf3e
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-07 05:50:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const enableBoxes = () => {
 }
 
 const showWinner = (winner) => {
-    msg.innerText = 'Congratulations, Winner is ${Winner}';
+    msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disabledBoxes();
 }
