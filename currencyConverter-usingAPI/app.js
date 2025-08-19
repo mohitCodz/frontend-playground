5e5aab5badbc4f6cc4140b72cea4605a6801f2a5
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 22:51:40 +0530

    Update app.js

--- currencyConverter-usingAPI/app.js
+++ currencyConverter-usingAPI/app.js
@@ -2,9 +2,9 @@ const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/
 
 const dropdown = document.querySelector(".dropdown select");
 
-let (code in countryList){
+for (code in countryList){
     console.log(code,countryList[code]);
 }
-for (let select of dropdown){
+// for (let select of dropdown){
 
-}
+// }
