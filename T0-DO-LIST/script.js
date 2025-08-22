08ff0266b9d66225bb27b3f031a619b6e4172f93
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-22 11:38:20 +0530

    Update script.js

--- T0-DO-LIST/script.js
+++ T0-DO-LIST/script.js
@@ -18,9 +18,11 @@ function addTask(){
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData(); // it will save the updated data
     } 
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData(); // it will save the updated data
     }
 }, false);
 
