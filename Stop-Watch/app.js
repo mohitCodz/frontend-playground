193dd644fa3e9e88abaede912f96abe3918ec20a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-23 13:45:38 +0530

    Update app.js

--- Stop-Watch/app.js
+++ Stop-Watch/app.js
@@ -1,1 +1,17 @@
-let [seconds,minutes,hours] = [0,0,0]; // array
+let [seconds, minutes, hours] = [0, 0, 0]; // array
+let displayTime = document.getElementById("displayTime");
+
+function stopWatch() {
+    seconds++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+            if (hours == 60) {
+                hours = 0;
+            }
+        }
+    }
+}
