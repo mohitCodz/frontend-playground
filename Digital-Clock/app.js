81472c5da83571778f6f9f6047e901bd4097e800
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-24 14:15:20 +0530

    Update app.js

--- Digital-Clock/app.js
+++ Digital-Clock/app.js
@@ -2,9 +2,10 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
+setInterval(()=>{
 let currentTime = new Date();
-// console.log(currentTime.getHours());
 
 hrs.innerHTML = currentTime.getHours(); // displaying hours
 min.innerHTML = currentTime.getMinutes(); // displaying minutes
 sec.innerHTML = currentTime.getSeconds(); // displaying seconds
+},1000)
